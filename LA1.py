0b4b3e4f7b494fcb67cb318c852a4925f2939a97
hsafdar2 <hsafdar2@gmu.edu>
2026-03-30 15:24:06 -0400

    Add files via upload

---
+++ LA1.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# LA1.py
+# Name:Huma Safdar
+# Python Version: 3.X
+#-------------------------------------------------------------------------------
+# Honor Code Statement: I received no assistance on this assignment that
+#                       violates the ethical guidelines as set forth by the
+#                       instructor and the class syllabus.
+#-------------------------------------------------------------------------------
+# References: 
+#-------------------------------------------------------------------------------
+# Comments to grader: 
+#-------------------------------------------------------------------------------
+# Code: Code starts here
+#-------------------------------------------------------------------------------
+
+    
+radius = input("enter radius of sphere:")
+
+pi = 3.14
+
+surface_area = 4 * pi *(float (radius) ** 2)
+
+print("The surface area of sphere is:", surface_area)
+
